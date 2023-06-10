abd123852dda498b16ffb0caebd9d44115c2edb4
daiki-kon <61902151+daiki-kon@users.noreply.github.com>
2023-06-10 22:18:30 +0900

    :lipstick: Kitchenのアイコン作成

---
+++ src/components/TablerToolsKitchen2Icon.tsx
@@ -0,0 +1,23 @@
+import type { SVGProps } from "react"
+
+
+export function TablerToolsKitchen2Icon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M19 3v12h-5c-.023-3.681.184-7.406 5-12zm0 12v6h-1v-3M8 4v17M5 4v3a3 3 0 1 0 6 0V4"
+      ></path>
+    </svg>
+  )
+}
