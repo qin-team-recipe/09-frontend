7e25c23e93589b68586e5bdf0d2999552bc9ce04
kabaji <kkojimawk@gmail.com>
2023-06-11 22:58:33 +0900

    css修正

--- src/app/components/RecipeItem.tsx
+++ src/app/components/RecipeItem.tsx
@@ -16,14 +16,14 @@ export const RecipeItem = (props: Props) => {
   return (
     <Link
       href={{ pathname: href }}
-      className="relative inline-block w-[174px] md:hover:opacity-90"
+      className="relative inline-block w-[174px] md:w-[218px] md:hover:opacity-90"
     >
       <Image src={image} width={400} height={400} alt="レシピ" />
       <div className="absolute right-2 top-2 flex items-center gap-1 rounded-2xl bg-[#0400137B]/[.48] px-2 py-1 text-sm font-normal text-white">
         <HeartIcon />
         {formattedNumber}
       </div>
-      <div className="mt-1 line-clamp-2 h-7 overflow-hidden text-xs font-bold text-[#1A1523]">
+      <div className="mt-1.5 line-clamp-2 h-7 overflow-hidden text-xs font-bold text-[#1A1523]">
         {title}
       </div>
       <div className="mt-1 line-clamp-1 overflow-hidden text-[10px] text-[#6F6E77]">
