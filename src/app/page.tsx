2b95986bd65c359cce8759d9efb8f94e249e9314
kabaji <kkojimawk@gmail.com>
2023-06-15 18:18:49 +0900

    調整

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,87 +25,90 @@ export type ChefType = {
   recipeCount: number
 }
 
+const featuredChefList: FeaturedChefType[] = [
+  {
+    name: "山田シェフ",
+    image: "/img/sampleChefTop.png",
+    path: "#",
+  },
+  {
+    name: "田中シェフ",
+    image: "/img/sampleChefTop.png",
+    path: "#",
+  },
+  {
+    name: "佐藤シェフ",
+    image: "/img/sampleChefTop.png",
+    path: "#",
+  },
+]
+
+const trendingRecipeList: TrendingRecipeType[] = [
+  {
+    title: "トマトとルッコラのマルゲリータピザに合うホワイトソースグラタン",
+    description: "ウルトラハイパー超すごいしまぶーシェフ",
+    favoriteCount: 1,
+    image: "/img/sampleRecipe.png",
+    path: "#",
+  },
+  {
+    title: "トマトとルッコラのマルゲリータピザ",
+    description: "ウルトラハイパー超すごいしまぶーシェフ",
+    favoriteCount: 1234,
+    image: "/img/sampleRecipe.png",
+    path: "#",
+  },
+  {
+    title: "トマトとルッコラ",
+    description: "すごいしまぶーシェフ",
+    favoriteCount: 123,
+    image: "/img/sampleRecipe.png",
+    path: "#",
+  },
+  {
+    title: "トマトとルッコラのマルゲリータピザに合うホワイトソースグラタン",
+    description: "ウルトラハイパー超すごいしまぶーシェフ",
+    favoriteCount: 12345,
+    image: "/img/sampleRecipe.png",
+    path: "#",
+  },
+  {
+    title: "トマトとルッコラのマルゲリータピザに合うホワイトソースグラタン",
+    description: "ウルトラハイパー超すごいしまぶーシェフ",
+    favoriteCount: 0,
+    image: "/img/sampleRecipe.png",
+    path: "#",
+  },
+]
+
+const chefList: ChefType[] = [
+  {
+    name: "ひふみシェフ",
+    description:
+      "白ごはん.comを運営しています。アップしたレシピの紹介や、youtube動画、日々の食のこと、オリジナル商品などの案内等をブログでやっています。",
+    image: "/img/sampleChefCard.png",
+    path: "#",
+    recipeCount: 123,
+  },
+  {
+    name: "ひふみシェフ",
+    description:
+      "白ごはん.comを運営しています。アップしたレシピの紹介や、youtube動画、日々の食のこと、オリジナル商品などの案内等をブログでやっています。",
+    image: "/img/sampleChefCard.png",
+    path: "#",
+    recipeCount: 1234,
+  },
+  {
+    name: "ひふみシェフ",
+    description:
+      "白ごはん.comを運営しています。アップしたレシピの紹介や、youtube動画、日々の食のこと、オリジナル商品などの案内等をブログでやっています。",
+    image: "/img/sampleChefCard.png",
+    path: "#",
+    recipeCount: 0,
+  },
+]
+
 export default function Home() {
-  const featuredChefList: FeaturedChefType[] = [
-    {
-      name: "山田シェフ",
-      image: "/img/sampleChefTop.png",
-      path: "#",
-    },
-    {
-      name: "田中シェフ",
-      image: "/img/sampleChefTop.png",
-      path: "#",
-    },
-    {
-      name: "佐藤シェフ",
-      image: "/img/sampleChefTop.png",
-      path: "#",
-    },
-  ]
-  const trendingRecipeList: TrendingRecipeType[] = [
-    {
-      title: "トマトとルッコラのマルゲリータピザに合うホワイトソースグラタン",
-      description: "ウルトラハイパー超すごいしまぶーシェフ",
-      favoriteCount: 1,
-      image: "/img/sampleRecipe.png",
-      path: "#",
-    },
-    {
-      title: "トマトとルッコラのマルゲリータピザ",
-      description: "ウルトラハイパー超すごいしまぶーシェフ",
-      favoriteCount: 1234,
-      image: "/img/sampleRecipe.png",
-      path: "#",
-    },
-    {
-      title: "トマトとルッコラ",
-      description: "すごいしまぶーシェフ",
-      favoriteCount: 123,
-      image: "/img/sampleRecipe.png",
-      path: "#",
-    },
-    {
-      title: "トマトとルッコラのマルゲリータピザに合うホワイトソースグラタン",
-      description: "ウルトラハイパー超すごいしまぶーシェフ",
-      favoriteCount: 12345,
-      image: "/img/sampleRecipe.png",
-      path: "#",
-    },
-    {
-      title: "トマトとルッコラのマルゲリータピザに合うホワイトソースグラタン",
-      description: "ウルトラハイパー超すごいしまぶーシェフ",
-      favoriteCount: 0,
-      image: "/img/sampleRecipe.png",
-      path: "#",
-    },
-  ]
-  const chefList: ChefType[] = [
-    {
-      name: "ひふみシェフ",
-      description:
-        "白ごはん.comを運営しています。アップしたレシピの紹介や、youtube動画、日々の食のこと、オリジナル商品などの案内等をブログでやっています。",
-      image: "/img/sampleChefCard.png",
-      path: "#",
-      recipeCount: 123,
-    },
-    {
-      name: "ひふみシェフ",
-      description:
-        "白ごはん.comを運営しています。アップしたレシピの紹介や、youtube動画、日々の食のこと、オリジナル商品などの案内等をブログでやっています。",
-      image: "/img/sampleChefCard.png",
-      path: "#",
-      recipeCount: 1234,
-    },
-    {
-      name: "ひふみシェフ",
-      description:
-        "白ごはん.comを運営しています。アップしたレシピの紹介や、youtube動画、日々の食のこと、オリジナル商品などの案内等をブログでやっています。",
-      image: "/img/sampleChefCard.png",
-      path: "#",
-      recipeCount: 0,
-    },
-  ]
   return (
     <div className="max-w-[480px] border-x border-[#EEDEF] pt-[57px]">
       <SearchBox />
