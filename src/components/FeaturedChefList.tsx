7781877924ec4fc9c16a80a3b0c1f37e344d5ad7
kabaji <kkojimawk@gmail.com>
2023-06-20 21:27:12 +0900

    PR FB対応

--- src/components/FeaturedChefList.tsx
+++ src/components/FeaturedChefList.tsx
@@ -16,9 +16,9 @@ export const FeaturedChefList = (props: Props) => {
         <HeadingTitle>注目のシェフ</HeadingTitle>
       </Heading>
       <div className="hidden-scrollbar mt-2.5 flex snap-x snap-mandatory scroll-pl-4 gap-4 overflow-x-auto scroll-smooth px-4">
-        {featuredChefList.map((chef, i) => {
-          return <FeaturedChef key={i} chef={chef} />
-        })}
+        {featuredChefList.map((chef, i) => (
+          <FeaturedChef key={i} chef={chef} />
+        ))}
       </div>
     </>
   )
