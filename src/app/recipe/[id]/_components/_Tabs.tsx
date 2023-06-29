29bf206cd5c54e7eeae3c6e743b3eb964f28ba54
daiki-kon <61902151+daiki-kon@users.noreply.github.com>
2023-06-29 22:13:12 +0900

    :lipstick: Tabsコンポーネント作成

---
+++ src/app/recipe/[id]/_components/_Tabs.tsx
@@ -0,0 +1,20 @@
+import { Children, cloneElement, ReactElement } from "react"
+
+type Props = {
+  activePath: string
+  children: ReactElement[]
+}
+
+export const Tabs = ({ activePath, children }: Props) => {
+  const activeClassName = "border-b-2 border-[#1A1523] font-bold "
+
+  return (
+    <nav className="flex [&>a]:flex-1 [&>a]:py-2.5 [&>a]:text-center">
+      {Children.map(children, (child: ReactElement) =>
+        cloneElement(child, {
+          className: activePath === child.props.href ? activeClassName : "",
+        }),
+      )}
+    </nav>
+  )
+}
