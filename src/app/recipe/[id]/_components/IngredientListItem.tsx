4e16253443a124e12242b13fa29bb0ac73ee4914
daiki-kon <61902151+daiki-kon@users.noreply.github.com>
2023-06-30 21:03:49 +0900

    IngredientListItemをクライアントコンポーネントに変更

--- src/app/recipe/[id]/_components/IngredientListItem.tsx
+++ src/app/recipe/[id]/_components/IngredientListItem.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { TablerShoppingCartPlus } from "@/components/icon/TablerShoppingCartPlus"
 
 type Props = {
@@ -8,6 +10,7 @@ export const IngredientListItem = ({ name }: Props) => {
   return (
     <div className="flex items-center border-b py-2 pl-4 pr-2">
       <p>{name}</p>
+      {/* TODO: カートに追加したときの処理を書く */}
       <button className="ml-auto rounded-full p-2 hover:bg-gray-300">
         <TablerShoppingCartPlus height={20} width={20} stroke="#6F6E77" />
       </button>
