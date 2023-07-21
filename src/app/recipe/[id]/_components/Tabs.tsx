67f5b09cd5511cf7ef2a3369c950e59f41b4bade
daiki-kon <61902151+daiki-kon@users.noreply.github.com>
2023-07-22 00:43:45 +0900

    重複していたスタイルを一箇所に

--- src/app/recipe/[id]/_components/Tabs.tsx
+++ src/app/recipe/[id]/_components/Tabs.tsx
@@ -6,14 +6,14 @@ type Props = {
 }
 
 export const Tabs = ({ activePath, children }: Props) => {
-  const activeClassName = "border-b-2 border-[#1A1523] font-bold"
-  const nonActiveClassName = "border-b-2 border-[#E4E2E4]"
+  const activeClassName = "border-[#1A1523] font-bold"
+  const nonActiveClassName = "border-[#E4E2E4]"
 
   return (
     <nav className="flex">
       {Children.map(children, (child: ReactElement) =>
         cloneElement(child, {
-          className: `flex-1 ${child.props.className} ${
+          className: `flex-1 border-b-2 ${child.props.className} ${
             activePath === child.props.href
               ? activeClassName
               : nonActiveClassName
