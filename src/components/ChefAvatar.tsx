2d2ec87c4aed43222bb79fae479a8550312f9c90
hiranuma <shuuuya0616@gmail.com>
2023-07-26 14:44:43 +0900

    リンクを追加

--- src/components/ChefAvatar.tsx
+++ src/components/ChefAvatar.tsx
@@ -1,25 +1,27 @@
 import Image from "next/image"
+import Link from "next/link"
 import type { FC } from "react"
+import {  ChefType } from "@/types/types"
 
 type Props = {
-  name: string
-  image: string
+  chef: ChefType
 }
 
-export const ChefAvatar: FC<Props> = (props) => {
-  const { name, image } = props
+export const ChefAvatar: FC<Props> = ({chef}) => {
 
   return (
-    <div className="flex flex-col items-center">
-      <div className="relative h-20 w-20 overflow-hidden rounded-full">
-        <Image
-          src={image}
-          alt="chef"
-          width={80}
-          height={80}
-        />
-      </div>
-      <p className="mt-2 text-center text-sm ">{name}</p>
-    </div>
+    <Link
+      href={{ pathname: chef.path }}
+      className={`flex flex-col items-center md:hover:opacity-90`}>
+        <div className="relative h-20 w-20 overflow-hidden rounded-full">
+          <Image
+            src={chef.image}
+            alt="chef"
+            width={80}
+            height={80}
+          />
+        </div>
+        <p className="mt-2 text-center text-sm ">{chef.name}</p>
+    </Link>
   )
 }
